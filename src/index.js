6db4a72db4e29a0660085c515a5800468c017aff
feoalexei <feoalexei@ukr.net>
2020-04-29 10:39:16 +0300

    swapi works again)))))

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
+/*eslint-disable*/
+
+'use strict';
 const $block = document.querySelector('.block');
 
-fetch('https://jsonplaceholder.typicode.com/users/')
+fetch('https://swapi.dev/api/films/')
     .then(response => response.json())
-    .then(users => {
-        for(const item in users) {
-            const $users = document.createElement('div');
-            $users.classList.add('username');
-            $block.append( $users);
-            $users.append(users[item].name);
+    .then(film => {
+        for(let i = 0; i < film.results.length; i++) {
+            const $films = document.createElement('div');
+            $films.classList.add('data-films');
+            $block.append($films);
+            $films.append(film.results[i].title);
 
-            $users.addEventListener('click', () => {
-                fetch('https://jsonplaceholder.typicode.com/users/')
+            function showStarShip() {
+                fetch('https://swapi.dev/api/films/')
                     .then(response => response.json())
-                    .then(user => {
-                        const $userInfo = document.createElement('span');
-                        $userInfo.classList.add('userdata');
-                        $users.after($userInfo);
-                        $userInfo.innerText = `lives in ${user[item].address.city},
-                                                    works at ${user[item].company.name},
-                                                    phone: ${user[item].phone}`;
-                    });
-            });
+                    .then(film => {
+                        for (let j = 0; j < film.results[j].starships.length; j++) {
+                            const $ships = document.createElement('span');
+                            $ships.classList.add('data-ships');
+                            $films.after($ships);
+                            fetch(`${film.results[i].starships[j]}`)
+                                .then(response => response.json())
+                                .then(ship => $ships.append(ship.name))                              
+                        }
+                    })
+                $films.removeEventListener('click', showStarShip);
+            }
+
+            $films.addEventListener('click', showStarShip);
+
         }
     });
